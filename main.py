a96863253f2f71d0016048bc796a395e8435b224
Roman <71276897+romangoempire@users.noreply.github.com>
2023-05-05 01:59:21 +0200

    feat: changed main.py that it has a clean entry point for the program.

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import cv2
+import numpy as np
+
+points = [(200, 200), (800, 200), (800, 450), (200, 450)]
+points_transformed = [[0, 0], [600, 0], [600, 600], [0, 600]]
+selected_point = None
+dragging = False
+M = None
 
 
-# Define the mouse callback function
+def initialize_cam():
+    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920 / 2)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080 / 2)
+    return cap
+
+
 def move_point(event, x, y, flags, param):
-    global points, selected_point, dragging
+    global points, selected_point, dragging, M
     if event == cv2.EVENT_LBUTTONDOWN:
         # Check if the mouse click is inside one of the points
         for i, (px, py) in enumerate(points):
@@ -14,48 +27,47 @@ def move_point(event, x, y, flags, param):
     elif event == cv2.EVENT_MOUSEMOVE and dragging:
         # Move the selected point
         points[selected_point] = (x, y)
-        # Redraw the lines between the points
-        for i in range(len(points)):
-            cv2.line(frame, points[i], points[(i + 1) % len(points)], (255, 0, 0), 2)
+        # Compute the perspective transformation matrix
+        if len(points) == 4:
+            src = np.float32(points)
+            dst = np.float32(points_transformed)
+            M = cv2.getPerspectiveTransform(src, dst)
     elif event == cv2.EVENT_LBUTTONUP:
         # Stop dragging
         dragging = False
 
 
-# Initialize the camera
-cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920 / 2)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080 / 2)
-
-# Initialize the points
-points = [(50, 50), (50, 200), (200, 50), (200, 200)]
-selected_point = None
-dragging = False
-
-# Set the mouse callback function
-cv2.namedWindow('camera')
-cv2.setMouseCallback('camera', move_point)
-
-while True:
-    # Read a frame from the camera
-    ret, frame = cap.read()
-
-    # Draw the lines between the points
+def draw_rectangle_with_corner_points():
     for i in range(len(points)):
         cv2.line(frame, points[i], points[(i + 1) % len(points)], (255, 0, 0), 2)
-
-    # Draw the points on the frame
     for px, py in points:
         cv2.circle(frame, (px, py), 5, (0, 255, 0), -1)
 
-    # Display the frame
-    cv2.imshow('camera', frame)
 
-    # Check for key presses
-    key = cv2.waitKey(1)
-    if key == ord('q'):
-        break
+if __name__ == '__main__':
 
-# Release the camera and close the window
-cap.release()
-cv2.destroyAllWindows()
+    cap = initialize_cam()
+
+    # Set the mouse callback function
+    cv2.namedWindow('camera')
+    cv2.setMouseCallback('camera', move_point)
+    transformed = None
+    while True:
+
+        ret, frame = cap.read()
+
+        if M is not None:
+            transformed = cv2.warpPerspective(frame, M, (600, 600))
+            cv2.imshow('transformed', transformed)
+
+        draw_rectangle_with_corner_points()
+
+        cv2.imshow('camera', frame)
+
+        # Check for key presses
+        key = cv2.waitKey(1)
+        if key == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
