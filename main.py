197159f3e52ee271ed8c9b54552243ad77a68a87
Roman <71276897+romangoempire@users.noreply.github.com>
2023-05-06 01:58:44 +0200

    feat: added output for positions that got changed by the move

--- main.py
+++ main.py
@@ -17,8 +17,9 @@ selected_point = None
 dragging = False
 M = None
 
-MAX_SIZES = int(WINDOW_SIZE / 20)
-HALF_MAX_SIZE = int(MAX_SIZES / 2)
+MAX_SIZE_TILE = int(WINDOW_SIZE / 20)
+print(MAX_SIZE_TILE)
+HALF_MAX_SIZE = int(MAX_SIZE_TILE / 2)
 
 max_value = 255
 max_sharpen = 30
@@ -36,8 +37,9 @@ black_alpha_value = 15
 black_beta_value = 0
 
 # black = 1, white = 2
-initial_board = np.zeros((ROWS, COLS), dtype='int16')
-board = initial_board.copy()
+initial_board = np.zeros((ROWS, COLS), dtype=np.uint8)
+last_board = np.zeros((ROWS, COLS), dtype=np.uint8)
+recording_board = initial_board.copy()
 next_board = initial_board.copy()
 frame_counter = 0
 
@@ -91,10 +93,10 @@ def on_threshold(val):
 
 
 def calculate_positions():
-    all_sizes = np.zeros((MAX_SIZES + 1, ROWS, COLS, 2), dtype='int16')
+    all_sizes = np.zeros((MAX_SIZE_TILE + 1, ROWS, COLS, 2), dtype='int16')
     h, w = WINDOW_SIZE, WINDOW_SIZE
     dy, dx = int((h - (h / 20)) / ROWS), int((w - (w / 20)) / COLS)
-    for i in range(MAX_SIZES + 1):
+    for i in range(MAX_SIZE_TILE + 1):
         offset = HALF_MAX_SIZE - int(round(i / 2))
         for j in range(ROWS):
             for k in range(COLS):
@@ -107,20 +109,23 @@ ALL_GRID_POINTS = calculate_positions()
 
 
 def create_all_slider():
-    cv2.namedWindow('White Image')
+    cv2.namedWindow('White Image', )
+    cv2.resizeWindow("White Image", 400, 200)
     cv2.createTrackbar('threshold_white', 'White Image', white_threshold, max_value, on_threshold)
     cv2.createTrackbar('sharpen', 'White Image', white_sharpen, max_sharpen, on_threshold)
     cv2.createTrackbar('alpha', 'White Image', white_alpha_value, max_alpha, on_threshold)
     cv2.createTrackbar('beta', 'White Image', white_beta_value, max_beta, on_threshold)
 
     cv2.namedWindow('Black Image')
+    cv2.resizeWindow("Black Image", 400, 200)
     cv2.createTrackbar('threshold_black', 'Black Image', black_threshold, max_value, on_threshold)
     cv2.createTrackbar('sharpen', 'Black Image', black_sharpen, max_sharpen, on_threshold)
     cv2.createTrackbar('alpha', 'Black Image', black_alpha_value, max_alpha, on_threshold)
     cv2.createTrackbar('beta', 'Black Image', black_beta_value, max_beta, on_threshold)
 
     cv2.namedWindow('Grid Image')
-    cv2.createTrackbar('size', 'Grid Image', size, MAX_SIZES, on_threshold)
+    cv2.resizeWindow("Grid Image", 400, 200)
+    cv2.createTrackbar('size', 'Grid Image', size, MAX_SIZE_TILE, on_threshold)
 
 
 def get_white(image):
@@ -183,8 +188,8 @@ def color_identifed_grid_greyscale(img, isBlack):
         for col in range(COLS):
             x = ALL_GRID_POINTS[size][row][col][0]
             y = ALL_GRID_POINTS[size][row][col][1]
-            max_x = ALL_GRID_POINTS[MAX_SIZES][row][col][0]
-            max_y = ALL_GRID_POINTS[MAX_SIZES][row][col][1]
+            max_x = ALL_GRID_POINTS[MAX_SIZE_TILE][row][col][0]
+            max_y = ALL_GRID_POINTS[MAX_SIZE_TILE][row][col][1]
 
             if size != 0:
                 square = img[y:y + size, x:x + size]
@@ -193,11 +198,12 @@ def color_identifed_grid_greyscale(img, isBlack):
                 color = closest_color(color)
 
                 if color == 255:
-                    board[row][col] = 1 if isBlack else 2
+                    recording_board[row][col] = 1 if isBlack else 2
 
-            cv2.rectangle(new_img, (max_x, max_y), (max_x + MAX_SIZES, max_y + MAX_SIZES), color=color,
+            cv2.rectangle(new_img, (max_x, max_y), (max_x + MAX_SIZE_TILE, max_y + MAX_SIZE_TILE), color=color,
                           thickness=-1)
-            cv2.rectangle(new_img, (max_x, max_y), (max_x + MAX_SIZES, max_y + MAX_SIZES), color=127, thickness=1)
+            cv2.rectangle(new_img, (max_x, max_y), (max_x + MAX_SIZE_TILE, max_y + MAX_SIZE_TILE), color=127,
+                          thickness=1)
     return new_img
 
 
@@ -205,33 +211,42 @@ def closest_color(color):
     return 255 if color >= 127 else 0
 
 
+def get_board_grid(img):
+    for i in range(ROWS + 1):
+        distance = i * MAX_SIZE_TILE
+        cv2.line(img, (MAX_SIZE_TILE, distance), (WINDOW_SIZE - MAX_SIZE_TILE, distance),
+                 color_black, 1)
+        cv2.line(img, (distance, MAX_SIZE_TILE), (distance, WINDOW_SIZE - MAX_SIZE_TILE),
+                 color_black, 1)
+    return img
+
+
 def drawn_board(img):
     new_img = np.zeros_like(img)
 
     cv2.rectangle(new_img, (0, 0), (WINDOW_SIZE, WINDOW_SIZE), color_brown, -1)
 
+    new_img = get_board_grid(new_img)
+
     for row in range(19):
         for col in range(19):
-            if board[row][col] == 1:
+            if recording_board[row][col] == 1:
                 board_color = color_black
-            elif board[row][col] == 2:
+            elif recording_board[row][col] == 2:
                 board_color = color_white
             else:
                 continue
-
-            max_x = ALL_GRID_POINTS[MAX_SIZES][row][col][0]
-            max_y = ALL_GRID_POINTS[MAX_SIZES][row][col][1]
+            max_x = ALL_GRID_POINTS[MAX_SIZE_TILE][row][col][0]
+            max_y = ALL_GRID_POINTS[MAX_SIZE_TILE][row][col][1]
             cv2.circle(new_img, (max_x + HALF_MAX_SIZE, max_y + HALF_MAX_SIZE), HALF_MAX_SIZE, board_color, -1)
     return new_img
 
 
 def is_valid_board():
     global frame_counter, next_board
-    print(np.array_equal(board, next_board), frame_counter, is_blacks_turn, count_black(), black_stones, count_white(),
-          white_stones)
-    if not np.array_equal(board, next_board):
+    if not np.array_equal(recording_board, next_board):
         frame_counter = 0
-        next_board = board.copy()
+        next_board = recording_board.copy()
         return False
     if frame_counter < 10:
         frame_counter += 1
@@ -241,22 +256,28 @@ def is_valid_board():
 
 
 def update_board():
-    global black_stones, white_stones, is_blacks_turn
+    global black_stones, white_stones, is_blacks_turn, last_board
     if is_blacks_turn:
         black_stones += 1
         is_blacks_turn = False
     else:
         white_stones += 1
         is_blacks_turn = True
-    print(is_blacks_turn, white_stones, black_stones)
+    get_last_move()
+    last_board = next_board.copy()
 
 
 def count_black():
-    return np.count_nonzero(board == 1)
+    return np.count_nonzero(recording_board == 1)
 
 
 def count_white():
-    return np.count_nonzero(board == 2)
+    return np.count_nonzero(recording_board == 2)
+
+
+def get_last_move():
+    row, col = np.where(last_board != next_board)
+    print(row - 1, col -1)
 
 
 if __name__ == '__main__':
@@ -278,7 +299,7 @@ if __name__ == '__main__':
     align = None
     final_board = None
     while True:
-        board = initial_board.copy()
+        recording_board = initial_board.copy()
         ret, frame = cap.read()
         draw_rectangle_with_corner_points()
 
@@ -286,11 +307,11 @@ if __name__ == '__main__':
             transformed = cv2.warpPerspective(frame, M, (WINDOW_SIZE, WINDOW_SIZE))
             white = get_white(transformed)
             black = get_black(transformed)
-            size = cv2.getTrackbarPos('size', 'Grid Image')
             align = draw_green_point_grid(transformed)
             identified_black = color_identifed_grid_greyscale(black, True)
             identified_white = color_identifed_grid_greyscale(white, False)
 
+            size = cv2.getTrackbarPos('size', 'Grid Image')
             if analyzing and is_valid_board():
                 update_board()
                 final_board = drawn_board(transformed)
@@ -309,6 +330,10 @@ if __name__ == '__main__':
         key = cv2.waitKey(1)
 
         if key == ord('s'):
+            if analyzing:
+                print('stop analysis')
+            else:
+                print('start analysis')
             analyzing = not analyzing
 
         if key == ord('q'):
